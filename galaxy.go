b77373314255d754d1606fce389b1ff25e77dfbd
Alice Di Nunno <alice@dinunno.fr>
2023-07-15 10:04:19 +0200

    debug register

--- galaxy.go
+++ galaxy.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/yeencloud/ServiceCore/decompose"
 	"github.com/yeencloud/ServiceCore/domain"
 )
@@ -20,15 +21,24 @@ type RegisterResponse struct {
 }
 
 func (sh *ServiceHost) register(host string, port int) error {
-	data, err := sh.callWithAddress(sh.Config.GetGalaxyAddress(), sh.Config.GetGalaxyPort(), "Galaxy", "Register", RegisterRequest{
+
+	registerRequest := RegisterRequest{
 		fmt.Sprintf("%s:%d", host, port),
 		*sh.serviceContent,
 		domain.APIVersion,
-	})
+	}
 
-	m, _ := json.Marshal(data)
+	spew.Dump("register request", registerRequest)
+	m, _ := json.Marshal(registerRequest)
+	spew.Dump(m)
+
+	data, err := sh.callWithAddress(sh.Config.GetGalaxyAddress(), sh.Config.GetGalaxyPort(), "Galaxy", "Register", registerRequest)
+
+	m, _ = json.Marshal(data)
 	var Response RegisterResponse
 	_ = json.Unmarshal(m, &Response)
+
+	spew.Dump("register reply", m)
 
 	if err != nil {
 		return errors.New(err.String)
