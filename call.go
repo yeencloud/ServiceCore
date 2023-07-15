44d6aaf7ef2485b2ad37aefd910520562b7fcb74
Alice Di Nunno <alice@dinunno.fr>
2023-07-15 22:57:15 +0200

    debug call

--- call.go
+++ call.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/yeencloud/ServiceCore/domain"
 	"github.com/yeencloud/ServiceCore/serviceError"
 	"github.com/yeencloud/ServiceCore/tools"
@@ -12,8 +13,10 @@ import (
 	"os"
 )
 
-func (sh *ServiceHost) callWithAddress(hostname string, port int, service string, method string, args any) (map[string]interface{}, *serviceError.Error) {
-	requestURL := fmt.Sprintf("http://%s:%d/rpc/", hostname, port)
+func (sh *ServiceHost) callWithAddress(address string, port int, service string, method string, args any) (map[string]interface{}, *serviceError.Error) {
+	requestURL := fmt.Sprintf("http://%s:%d/rpc/", address, port)
+
+	spew.Dump("Call at ", requestURL)
 
 	callData := tools.AnyToMap(args)
 
@@ -26,17 +29,20 @@ func (sh *ServiceHost) callWithAddress(hostname string, port int, service string
 
 	req, err := http.NewRequest(http.MethodPost, requestURL, bytes.NewBuffer(j))
 	if err != nil {
+		spew.Dump("Call error", err)
 		fmt.Printf("client: could not create request: %s\n", err)
 		os.Exit(1)
 	}
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
+		spew.Dump("Call error", err)
 		return nil, serviceError.Trace(ErrCallCouldNotReadResponseBody) //.Embed(err)
 	}
 
 	resBody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
+		spew.Dump("Call error", err)
 		return nil, serviceError.Trace(ErrCallCouldNotReadResponseBody)
 	}
 
@@ -44,6 +50,7 @@ func (sh *ServiceHost) callWithAddress(hostname string, port int, service string
 	err = json.Unmarshal(resBody, &response)
 
 	if err != nil {
+		spew.Dump("Call error", err)
 		return nil, serviceError.Trace(ErrCallCouldNotUnmarshalResponseBody)
 	}
 
@@ -60,12 +67,14 @@ func (sh *ServiceHost) Call(service string, method string, args any) (map[string
 		port = sh.Config.GetGalaxyPort()
 	} else {
 		lookup, err := sh.LookUp(service, method)
-		address = lookup.Address
 		if err != nil {
 			return nil, err
 		}
+		address = lookup.Address
 		port = lookup.Port
 	}
+
+	spew.Dump("service", service, "method", method, " is at ", address, ":", port)
 
 	return sh.callWithAddress(address, port, service, method, args)
 }
